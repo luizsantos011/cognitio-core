f34a51b82c4fdcfe157b92c5020a9c056cb591ff
luiz <luizsantost4@gmail.com>
2026-07-07 16:48:15 -0300

    feat: adicionar estrutura base e codec do InsightData

---
+++ src/main/java/com/cognitio/InsightData.java
@@ -0,0 +1,10 @@
+package com.cognitio;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+public record InsightData(int points) {
+    public static final Codec<InsightData> CODEC = RecordCodecBuilder.create(instance ->
+            instance.group(Codec.INT.fieldOf("points").forGetter(InsightData::points)).apply(instance, InsightData::new)
+    );
+}
