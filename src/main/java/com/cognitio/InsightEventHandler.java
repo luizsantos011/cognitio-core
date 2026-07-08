e6737d9cb2e4b7a94684403b810b654ecd63d37f
luiz <luizsantost4@gmail.com>
2026-07-08 16:01:42 -0300

    feat: implementar teste de drop de semente baseado na perspectiva minima

--- src/main/java/com/cognitio/InsightEventHandler.java
+++ src/main/java/com/cognitio/InsightEventHandler.java
@@ -22,4 +22,23 @@ public class InsightEventHandler {
             player.sendSystemMessage(Component.literal("Você ganhou 1 ponto de Insight! Total: " + newPoints));
         }
     }
+
+    @SubscribeEvent
+    public static void onBlockDrop(BlockEvent.BreakEvent event) {
+        Player player = event.getPlayer();
+
+        if(!event.getLevel().isClientSide() && event.getState().is(net.minecraft.world.level.block.Blocks.GRASS_BLOCK)) {
+            int currentPerspective = PerspectiveEngine.getEffectivePerspective(player);
+
+            if(currentPerspective >= 10){
+                net.minecraft.world.level.Level level = (net.minecraft.world.level.Level) event.getLevel();
+                net.minecraft.core.BlockPos pos = event.getPos();
+                net.minecraft.world.entity.item.ItemEntity seedDrop = new net.minecraft.world.entity.item.ItemEntity(
+                        level,
+                        pos.getX(), pos.getY(), pos.getZ(),
+                        new net.minecraft.world.item.ItemStack(net.minecraft.world.item.Items.WHEAT_SEEDS, 1));
+                level.addFreshEntity(seedDrop);
+            }
+        }
+    }
 }
