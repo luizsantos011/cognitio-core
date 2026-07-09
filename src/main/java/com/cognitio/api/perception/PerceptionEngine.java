8923471fc8c125c251f264344529cab83bfbb831
luiz <luizsantost4@gmail.com>
2026-07-09 11:55:12 -0300

    feat: adicionar metodos de escrita para aplicar e remover modificadores na PerceptionEngine

--- src/main/java/com/cognitio/api/perception/PerceptionEngine.java
+++ src/main/java/com/cognitio/api/perception/PerceptionEngine.java
@@ -4,6 +4,9 @@ import com.cognitio.attachment.AttachmentRegister;
 import com.cognitio.attachment.InsightData;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PerceptionEngine {
 
     public static int getEffectivePerception(Player player) {
@@ -32,5 +35,29 @@ public class PerceptionEngine {
             total += bonus;
         }
         return total;
+    }
+
+    public static void applyModifier(Player player, String id, double multiplier, int bonus) {
+        InsightData oldData = player.getData(AttachmentRegister.COGNITIO_INSIGHT.get());
+        Map<String, Double> newMultipliers = new HashMap<>(oldData.multipliers());
+        Map<String, Integer> newBonuses = new HashMap<>(oldData.bonuses());
+
+        newMultipliers.put(id, multiplier);
+        newBonuses.put(id, bonus);
+
+        InsightData newData = new InsightData(oldData.points(), newMultipliers, newBonuses);
+        player.setData(AttachmentRegister.COGNITIO_INSIGHT.get(), newData);
+    }
+
+    public static void removeModifier(Player player, String id) {
+        InsightData oldData = player.getData(AttachmentRegister.COGNITIO_INSIGHT.get());
+        Map<String, Double> newMultipliers = new HashMap<>(oldData.multipliers());
+        Map<String, Integer> newBonuses = new HashMap<>(oldData.bonuses());
+
+        newMultipliers.remove(id);
+        newBonuses.remove(id);
+
+        InsightData newData = new InsightData(oldData.points(), newMultipliers, newBonuses);
+        player.setData(AttachmentRegister.COGNITIO_INSIGHT.get(), newData);
     }
 }
